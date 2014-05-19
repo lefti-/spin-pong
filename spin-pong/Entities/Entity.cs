0f78e456ed85ccfff073dc43396275f1a32a7911
lefti <jes.bergstrom@gmail.com>
2014-05-19 17:55:26 +0300

    Organizing project

---
+++ spin-pong/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿/*
+    Spin Pong
+    This software uses The MIT License (MIT). See license agreement LICENSE for full details.
+*/
+
+namespace Entities
+{
+    public class Entity
+    {
+        // Box2D doesn't use pixels as units, but meters.
+        // To work correctly with pixels, convert meters to pixels.
+        public const float PixelsToMeter = 32.0f;
+    }
+}
---
+++ spin-pong/GameStates/GameState.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Spin Pong
+    This software uses The MIT License (MIT). See license agreement LICENSE for full details.
+*/
+
+using SFML.Graphics;
+
+namespace GameStates
+{
+    public abstract class GameState
+    {
+        public GameState() { }
+        public virtual void Initialize(RenderWindow window) { }
+        public virtual void BindEvents(RenderWindow window) { }
+        public virtual void UnbindEvents(RenderWindow window) { }
+        public virtual void Update(RenderWindow window) { }
+        public virtual void Draw(RenderWindow window) { }
+    }
+}
